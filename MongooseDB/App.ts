e11c5dd3a87289d90b0fce63da424562a953e48a
Emily Fang Bazar <emilyfbazar@gmail.com>
2024-10-30 16:55:09 -0700

    Added endpoints for CRUD recipe

--- MongooseDB/App.ts
+++ MongooseDB/App.ts
@@ -1,113 +1,97 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import {RecipeModel} from './model/RecipeModel';
-import {ModifiedRecipeModel} from './model/ModifiedRecipeModel';
-import {CookbookModel} from './model/CookbookModel';
+import { RecipeModel } from './model/RecipeModel';
+import { ModifiedRecipeModel } from './model/ModifiedRecipeModel';
+import { CookbookModel } from './model/CookbookModel';
 import * as crypto from 'crypto';
 
-// Creates and configures an ExpressJS web server.
 class App {
-
-  // ref to Express instance
   public expressApp: express.Application;
-  public RecipeList:RecipeModel;
-  public Cookbook:CookbookModel;
+  public RecipeList: RecipeModel;
+  public Cookbook: CookbookModel;
+  public ModifiedRecipes: ModifiedRecipeModel;
 
-  //Run configuration methods on the Express instance.
-  constructor(mongoDBConnection:string)
-  {
+  constructor(mongoDBConnection: string) {
     this.expressApp = express();
     this.middleware();
     this.routes();
     this.RecipeList = new RecipeModel(mongoDBConnection);
-    // this.ModifiedRecipes = new ModifiedRecipeModel(mongoDBConnection);
+    this.ModifiedRecipes = new ModifiedRecipeModel(mongoDBConnection);
     this.Cookbook = new CookbookModel(mongoDBConnection);
   }
 
-  // Configure Express middleware.
   private middleware(): void {
     this.expressApp.use(bodyParser.json());
     this.expressApp.use(bodyParser.urlencoded({ extended: false }));
-    this.expressApp.use( (req, res, next) => {
+    this.expressApp.use((req, res, next) => {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
   }
 
-  // Configure API endpoints.
   private routes(): void {
     let router = express.Router();
 
-    // Get one recipe from recipe list by recipeID
-    router.get('/app/recipelist/:recipeID/', async (req, res) => {
-        var id = req.params.recipe_ID;
-        console.log('Query recipe list with id: ' + id);
-        await this.RecipeList.retrieveRecipe(res, id);
+    // Retrieve all recipes
+    router.get('/app/recipes', async (req, res) => {
+      await this.RecipeList.retrieveAllRecipes(res);
     });
 
-    //
-   /* router.get('/app/list/:listId', async (req, res) => {
-      var id = req.params.listId;
-      console.log('Query single list with id: ' + id);
-      await this.Lists.retrieveRecipe(res, id);
+    // Retrieve a specific recipe by recipe_ID
+    router.get('/app/recipes/:recipeID', async (req, res) => {
+      const recipeID = req.params.recipeID;
+      console.log('Query recipe list with id: ' + recipeID);
+      await this.RecipeList.retrieveRecipe(res, recipeID);
     });
 
-    router.post('/app/list/', async (req, res) => {
+    // Add a new recipe
+    router.post('/app/recipes', async (req, res) => {
       const id = crypto.randomBytes(16).toString("hex");
-      console.log(req.body);
-        var jsonObj = req.body;
-        jsonObj.listId = id;
-        try {
-          await this.Lists.model.create([jsonObj]);
-          res.send('{"id":"' + id + '"}');
-        }
-        catch (e) {
-          console.error(e);
-          console.log('object creation failed');
-        }
+      const jsonObj = { ...req.body, recipe_ID: id };
+      try {
+        await this.RecipeList.model.create(jsonObj);
+        res.json({ id });
+      } catch (e) {
+        console.error('Failed to create recipe:', e);
+        res.status(500).json({ error: "Failed to create recipe" });
+      }
     });
 
-    router.post('/app/list2/', async (req, res) => {
-      const id = crypto.randomBytes(16).toString("hex");
-      console.log(req.body);
-        var jsonObj = req.body;
-        jsonObj.listId = id;
-        const doc = new this.Lists.model(jsonObj);
-        try {
-          await doc.save();
-          res.send('{"id":"' + id + '"}');
-        }
-        catch (e) {
-          console.log('object creation failed');
-          console.error(e);
-        }        
+    // Update the directions of a recipe
+    router.put('/app/recipes/:recipeID/directions', async (req, res) => {
+      const recipeID = req.params.recipeID;
+      const { directions } = req.body;
+      await this.RecipeList.updateDirections(res, recipeID, directions);
     });
 
-    router.get('/app/list/:listId/tasks', async (req, res) => {
-        var id = req.params.listId;
-        console.log('Query single list with id: ' + id);
-        await this.Tasks.retrieveTasksDetails(res, {listId: id});
+    // Update a specific direction step of a recipe
+    router.put('/app/recipes/:recipeID/directions/:stepIndex', async (req, res) => {
+      const recipeID = req.params.recipeID;
+      const stepIndex = parseInt(req.params.stepIndex);
+      const { newStep } = req.body;
+      await this.RecipeList.updateDirectionStep(res, recipeID, stepIndex, newStep);
     });
 
-    router.get('/app/list/', async (req, res) => {
-        console.log('Query All list');
-        await this.Lists.retrieveAllLists(res);
+    // Update the ingredients of a recipe
+    router.put('/app/recipes/:recipeID/ingredients', async (req, res) => {
+      const recipeID = req.params.recipeID;
+      const { ingredients } = req.body;
+      await this.RecipeList.updateIngredients(res, recipeID, ingredients);
     });
 
-    router.get('/app/listcount', async (req, res) => {
-      console.log('Query the number of list elements in db');
-      await this.Lists.retrieveListCount(res);
+    // Delete a recipe by recipe_ID
+    router.delete('/app/recipes/:recipeID', async (req, res) => {
+      const recipeID = req.params.recipeID;
+      await this.RecipeList.deleteRecipe(res, recipeID);
     });
-*/
+
+    // Serve static files
     this.expressApp.use('/', router);
-
-    this.expressApp.use('/app/json/', express.static(__dirname+'/app/json'));
-    this.expressApp.use('/images', express.static(__dirname+'/img'));
-    this.expressApp.use('/', express.static(__dirname+'/pages'));
-    
+    this.expressApp.use('/app/json/', express.static(__dirname + '/app/json'));
+    this.expressApp.use('/images', express.static(__dirname + '/img'));
+    this.expressApp.use('/', express.static(__dirname + '/pages'));
   }
-
 }
 
-export {App};
+export { App };
