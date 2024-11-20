f2cca7624617b903e705a34e0608339467007413
becauliffe <98365801+becauliffe@users.noreply.github.com>
2024-11-20 13:28:13 -0800

    new interfaces on main

--- MongooseDB/interfaces/IRecipe.ts
+++ MongooseDB/interfaces/IRecipe.ts
@@ -1,25 +1,14 @@
 import { Document } from "mongoose";
 
 export interface IRecipe extends Document {
-    user_ID: string;
-    recipe_ID: string;
-    recipe_name: string;
-    category: string[]; 
-    cooking_duration: number;
-    ingredients: {
-        ingredient_id?: string;
-        name: string;
-        quantity: number;
-        unit: string;
-    }[];
-    directions: {
-        step: string;
-    }[];
-    image_url?: string;
-    is_visible?: boolean;
+  modified_flag: boolean;
+  user_ID: string;
+  recipe_ID: string;
+  recipe_name: string;
+  category: string[];
+  recipe_versions: [Object]; // THESE ARE THE RECIPE CONTENTS OBJECTS
+  image_url?: string;
+  is_visible?: boolean;
 
-    // Fields specific to ModifiedRecipeModel
-    personal_recipe_ID?: string;
-    notes?: string;
-    version_number?: number;
+  // Fields specific to ModifiedRecipeModel
 }
