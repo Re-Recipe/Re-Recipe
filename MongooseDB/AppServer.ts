6169b9b4d869bc2f3f08e906b70486d6d94d09f9
Emily Fang Bazar <emilyfbazar@gmail.com>
2024-11-22 16:52:00 -0800

    Updated workflow

--- MongooseDB/AppServer.ts
+++ MongooseDB/AppServer.ts
@@ -1,15 +1,25 @@
+const express = require('express');
+const path = require('path');
 import * as dotenv from 'dotenv';
 import { App } from './App';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const mongoDBConnection = process.env.DB_INFO;
 
 console.log("server db connection URL " + mongoDBConnection);
 console.log("process.env.DB_INFO " + process.env.DB_INFO);
 
 let server: any = new App(mongoDBConnection).expressApp;
+
+const angularDistPath = path.join(__dirname, '../frontend/recipes');
+server.use('/', express.static(angularDistPath));
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(angularDistPath, 'index.html'));
+});
+
 server.listen(port, () => {
-    console.log("server running on port " + port);
+    console.log(`server running on port ${port}`);
 });
