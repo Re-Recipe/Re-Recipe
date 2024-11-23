5b3c4f83ecf64dba741e02a1d723e37d8af0207d
myasha <myasha@gmail.com>
2024-11-23 13:46:49 -0800

    updated user interface in interfaces

--- MongooseDB/interfaces/IUser.ts
+++ MongooseDB/interfaces/IUser.ts
@@ -1,14 +1,13 @@
-// We might want to add or modify this schema 
-
 import Mongoose = require("mongoose");
 
 interface IUser extends Mongoose.Document {
-    user_ID: string; 
-    username: string; 
-    email: string; 
-    password: string; 
-    createdAt: Date; 
-    recipeIDs: Mongoose.Types.ObjectId[]; // array of recipe IDs associated with the user?
+  user_ID: string; // System generated & unique user ID
+  username: string; // Unique username, system-generated
+  email: string; // User's email address
+  password: string; // Hashed password
+  createdAt?: Date; // Optional if timestamps are auto-managed
+  hints?: string; // Optional hints for the user
+  recipeIDs: Mongoose.Types.ObjectId[]; // Array of ObjectIds referencing Recipe model
 }
 
 export { IUser };
