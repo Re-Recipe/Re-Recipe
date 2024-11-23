3f1de206cc9c79df2e567248c2fcfca905360120
Emily Fang Bazar <emilyfbazar@gmail.com>
2024-11-22 18:40:43 -0800

    Got rid of unnecessary files

--- MongooseDB/AppServer.js
+++ MongooseDB/AppServer.js
@@ -1,13 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var express = require('express');
+var path = require('path');
 var dotenv = require("dotenv");
 var App_1 = require("./App");
 dotenv.config();
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 var mongoDBConnection = process.env.DB_INFO;
 console.log("server db connection URL " + mongoDBConnection);
 console.log("process.env.DB_INFO " + process.env.DB_INFO);
 var server = new App_1.App(mongoDBConnection).expressApp;
+var angularDistPath = path.join(__dirname, '../frontend/recipes');
+server.use('/', express.static(angularDistPath));
+server.get('*', function (req, res) {
+    res.sendFile(path.join(angularDistPath, 'index.html'));
+});
 server.listen(port, function () {
-    console.log("server running on port " + port);
+    console.log("server running on port ".concat(port));
 });
