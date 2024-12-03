5f0165fc4886e0a67e8c0a06f70112737b14ccb6
myasha <myasha@gmail.com>
2024-12-03 15:42:46 -0800

    removed port 8080

--- MongooseDB/AppServer.ts
+++ MongooseDB/AppServer.ts
@@ -8,7 +8,7 @@ import { App } from './App';
 
 dotenv.config();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT
 const mongoDBConnection = process.env.DB_INFO;
 
 console.log("server db connection URL " + mongoDBConnection);
