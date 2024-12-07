2bbaf1b8c72a0ecb4d83a881cae8988977a43d5a
Emily Fang Bazar <emilyfbazar@gmail.com>
2024-12-06 17:43:06 -0800

    Refactored and fixed sso to work in .env

--- MongooseDB/googleOauth2.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-class googleOauth2 {
-  static id: string = process.env.GOOGLE_CLIENT_ID as string;
-  static secret: string = process.env.GOOGLE_CLIENT_SECRET as string;
-}
-
-export default googleOauth2;
