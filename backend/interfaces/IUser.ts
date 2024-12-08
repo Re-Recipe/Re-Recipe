0b431fb2fe1e2ac472ae8497017f229c41d7890d
myasha <myasha@gmail.com>
2024-12-08 14:12:21 -0800

    added field for display name

--- backend/interfaces/IUser.ts
+++ backend/interfaces/IUser.ts
@@ -3,7 +3,8 @@ import Mongoose = require("mongoose");
 interface IUser extends Mongoose.Document {
   user_ID: string; // SSO ID 
   email: string; // User's email address
-  color: string//
+  displayName: string;
+  color: string 
   recipeIDs: Mongoose.Types.ObjectId[]; // Array of ObjectIds referencing Recipe model
 }
 
